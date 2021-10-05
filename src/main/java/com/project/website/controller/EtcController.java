565c160c09d6ab039375a1b36e3d6000f7da745f
SangjunDev <snagjun.kim.dev@gmail.om>
2021-10-06 00:37:59 +0900

    Front Update

---
+++ src/main/java/com/project/website/controller/EtcController.java
@@ -0,0 +1,38 @@
+package com.project.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EtcController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about_page(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact_page(){
+        return "contact";
+    }
+
+    @GetMapping("/gallery")
+    public String gallery_page(){
+        return "gallery";
+    }
+
+    @GetMapping("/packages")
+    public String packages_page(){
+        return "packages";
+    }
+
+    @GetMapping("/pricing")
+    public String pricing_page(){
+        return "pricing";
+    }
+}
